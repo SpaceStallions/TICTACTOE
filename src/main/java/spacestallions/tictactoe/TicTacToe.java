1c7e08d2f1383480408415cfab32e3f89e32f0c7
BjossiAlfreds <bjorna11@ru.is>
2013-11-19 02:46:32 +0000

    Added jquery ref to index.html and added javascript that binds a click event to the 9 cells

--- src/main/java/spacestallions/tictactoe/TicTacToe.java
+++ src/main/java/spacestallions/tictactoe/TicTacToe.java
@@ -6,25 +6,27 @@ import spark.*;
 
 public class TicTacToe
 {
-	public String greet(){
+	public String greet() {
 		return "Welcome to TicTacToe!";
 	}
+
 	public static void main (String[] args)
 	{
 		staticFileLocation("/public");     
-        setPort(Integer.valueOf(System.getenv("PORT")));
-        get(new Route("/") {
-        	@Override
-        	public Object handle(Request request, Response response) {
-            	return "SpaceStallions TicTacToe";
-         	}
-        });
-		
+		setPort(Integer.valueOf(System.getenv("PORT")));
+
 		get(new Route("/") {
-        	@Override
-        	public Object handle(Request request, Response response) {
-            	return request.body();
-         	}
-        });
+			@Override
+			public Object handle(Request request, Response response) {
+				return "SpaceStallions TicTacToe";
+			}
+		});
+
+		get(new Route("/") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return request.body();
+			}
+		});
 	}
 }
