d4aba2632026c7a509991d68eee696831fa83eb7
joezombie <joezombie@gmail.com>
2013-11-19 18:27:21 +0000

    Added call infront of mvn command

--- bin/package_run.cmd
+++ bin/package_run.cmd
@@ -1,4 +1,4 @@
 @echo off
-mvn clean compile package
+call mvn clean compile package
 set PORT=4567
-java -jar target\TICTACTOE-1.0-SNAPSHOT-jar-with-dependencies.jar
+start "Spark TicTacToe" java -jar target\TICTACTOE-1.0-SNAPSHOT-jar-with-dependencies.jar
