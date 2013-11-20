cede680346010f59f3581213c5fac6a629452437
BjossiAlfreds <bjorna11@ru.is>
2013-11-20 03:37:50 +0000

    Added ajax call to server from the game board, added a new route /clickcell, added a special clickCell subroutine and a unit test for it

--- src/main/java/spacestallions/tictactoe/TicTacToe.java
+++ src/main/java/spacestallions/tictactoe/TicTacToe.java
@@ -6,6 +6,19 @@ import spark.*;
 
 public class TicTacToe
 {
+	// The entry point of the game logic, this is called from the /clickcell route below
+	public static Object clickCell (String gameState, int player, int cellNumber)
+	{
+		// TODO: add more subroutines (and respective unit tests) and statements for game logic
+		// We need to switch the player number between 1 and 2, determine whether clicking a cell was a valid move
+		// and of course detect a winning / losing / tie condition
+		// Finally wrap all that info into the json string and return to the ajax .done function and update the client side...
+
+		String json = "{ \"player\": " + Integer.toString (player) + ", \"valid\": \"true\", \"endGame\": 0 }";
+
+		return json;
+	}
+
 	public String greet() {
 		return "Welcome to TicTacToe!";
 	}
@@ -14,14 +27,29 @@ public class TicTacToe
 	{
 		staticFileLocation("/public");
 
-        // The port that we should run on can be set into an environment variable
-        // Look for that variable and default to 8080 if it isn't there.
-        String webPort =  System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()){
-            webPort = "4567";
-        }
-        setPort(Integer.valueOf(webPort));
+		// The port that we should run on can be set into an environment variable
+		// Look for that variable and default to 8080 if it isn't there.
+		String webPort =  System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()){
+			webPort = "4567";
+		}
 
+		setPort(Integer.valueOf(webPort));
+
+		// This route is called through AJAX when a player clicks a cell in the game board
+		// Returns a json string back to the done() function in the javascript part of index.html
+		get(new Route ("/clickcell")
+		{
+			@Override
+			public Object handle (Request req, Response res)
+			{
+				String gameState = req.queryParams ("state");
+				int player = Integer.valueOf (req.queryParams ("player"));
+				int cellNumber = Integer.valueOf (req.queryParams ("cell"));
+
+				return clickCell (gameState, player, cellNumber);
+			}
+		});
 
 		get(new Route("/gettest") {
 			@Override
--- src/test/java/spacestallions/tictactoe/TicTacToeTest.java
+++ src/test/java/spacestallions/tictactoe/TicTacToeTest.java
@@ -11,4 +11,12 @@ public class TicTacToeTest
 		TicTacToe ttt = new TicTacToe();
 		assertEquals("Welcome to TicTacToe!",ttt.greet());
 	}
+
+	@Test
+	public void testJsonFromClickCell ( )
+	{
+		String expected = "{ \"player\": 1, \"valid\": \"true\", \"endGame\": 0 }";
+
+		assertEquals (TicTacToe.clickCell ("000000000", 1, 1), expected);
+	}
 }
