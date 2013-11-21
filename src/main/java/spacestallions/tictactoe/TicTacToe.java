52a11012896a63c288987ef05099b4c78197568b
BjossiAlfreds <bjorna11@ru.is>
2013-11-21 04:11:08 +0000

    Implemented an alpha of the game, along with unit tests

--- src/main/java/spacestallions/tictactoe/TicTacToe.java
+++ src/main/java/spacestallions/tictactoe/TicTacToe.java
@@ -6,15 +6,57 @@ import spark.*;
 
 public class TicTacToe
 {
-	// The entry point of the game logic, this is called from the /clickcell route below
-	public static Object clickCell (String gameState, int player, int cellNumber)
+	public static char switchPlayer (char currentPlayer)
 	{
-		// TODO: add more subroutines (and respective unit tests) and statements for game logic
-		// We need to switch the player number between 1 and 2, determine whether clicking a cell was a valid move
-		// and of course detect a winning / losing / tie condition
-		// Finally wrap all that info into the json string and return to the ajax .done function and update the client side...
+		if (currentPlayer == 'X')
+			return 'O';
+		else
+			return 'X';
+	}
 
-		String json = "{ \"player\": " + Integer.toString (player) + ", \"valid\": \"true\", \"endGame\": 0 }";
+	// Detects whether the game has ended after this move is made
+	// Returns 1 if the current player has won, 2 if there is a tie and 0 if the game has not ended
+	public static int detectEndGame (char[] gameState)
+	{
+		// Let's scan over all the possible ways to win in Tic Tac Toe
+		if (((gameState[0] == gameState[1]) && (gameState[1] == gameState[2]) && gameState[2] != '0')
+		|| ((gameState[3] == gameState[4]) && (gameState[4] == gameState[5]) && gameState[5] != '0')
+		|| ((gameState[6] == gameState[7]) && (gameState[7] == gameState[8]) && gameState[8] != '0')
+		|| ((gameState[0] == gameState[3]) && (gameState[3] == gameState[6]) && gameState[6] != '0')
+		|| ((gameState[1] == gameState[4]) && (gameState[4] == gameState[7]) && gameState[7] != '0')
+		|| ((gameState[2] == gameState[5]) && (gameState[5] == gameState[8]) && gameState[8] != '0')
+		|| ((gameState[2] == gameState[4]) && (gameState[4] == gameState[6]) && gameState[6] != '0')
+		|| ((gameState[0] == gameState[4]) && (gameState[4] == gameState[8]) && gameState[8] != '0'))
+			return 1;
+
+		// No one has won yet? Let's see if there is a tie...
+		for (int i = 0 ; i < 9 ; i++)
+		{
+			if (gameState[i] == '0')
+				return 0;	// Nope, there is an unused cell left
+		}
+
+		// Yup, it's a tie
+		return 2;
+	}
+
+	// The entry point of the game logic, this is called from the /clickcell route below
+	public static Object clickCell (char[] gameState, char player, int cellNumber)
+	{
+		boolean validMove = false;
+		int endGame = 0;
+
+		if (gameState[cellNumber - 1] == '0')
+		{
+			validMove = true;
+			gameState[cellNumber - 1] = player;
+
+			endGame = detectEndGame (gameState);
+
+			player = switchPlayer (player);
+		}
+
+		String json = "{ \"nextPlayer\": \"" + String.valueOf (player) + "\", \"validMove\": " + String.valueOf (validMove) + ", \"endGame\": " + String.valueOf (endGame) + " }";
 
 		return json;
 	}
@@ -43,8 +85,8 @@ public class TicTacToe
 			@Override
 			public Object handle (Request req, Response res)
 			{
-				String gameState = req.queryParams ("state");
-				int player = Integer.valueOf (req.queryParams ("player"));
+				char[] gameState = req.queryParams ("state").toCharArray ( );
+				char player = req.queryParams ("player").charAt (0);
 				int cellNumber = Integer.valueOf (req.queryParams ("cell"));
 
 				return clickCell (gameState, player, cellNumber);
--- src/test/java/spacestallions/tictactoe/TicTacToeTest.java
+++ src/test/java/spacestallions/tictactoe/TicTacToeTest.java
@@ -15,8 +15,41 @@ public class TicTacToeTest
 	@Test
 	public void testJsonFromClickCell ( )
 	{
-		String expected = "{ \"player\": 1, \"valid\": \"true\", \"endGame\": 0 }";
+		String expected = "{ \"nextPlayer\": \"O\", \"validMove\": true, \"endGame\": 0 }";
 
-		assertEquals (TicTacToe.clickCell ("000000000", 1, 1), expected);
+		assertEquals (TicTacToe.clickCell ("000000000".toCharArray ( ), 'X', 1), expected);
+	}
+
+	@Test
+	public void testEndGameZero ( )
+	{
+		String gameState = "120010000";
+		assertEquals (TicTacToe.detectEndGame (gameState.toCharArray ( )), 0);
+	}
+
+	@Test
+	public void testEndGameOne ( )
+	{
+		String gameState = "200002111";
+		assertEquals (TicTacToe.detectEndGame (gameState.toCharArray ( )), 1);
+	}
+
+	@Test
+	public void testEndGameTwo ( )
+	{
+		String gameState = "112221121";
+		assertEquals (TicTacToe.detectEndGame (gameState.toCharArray ( )), 2);
+	}
+
+	@Test
+	public void testPlayerSwitchToOne ( )
+	{
+		assertEquals (TicTacToe.switchPlayer ('X'), 'O');
+	}
+
+	@Test
+	public void testPlayerSwitchToTwo ( )
+	{
+		assertEquals (TicTacToe.switchPlayer ('O'), 'X');
 	}
 }
