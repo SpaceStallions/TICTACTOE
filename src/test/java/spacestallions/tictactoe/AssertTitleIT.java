32d0a192c22196fa67790bea2dade97e5f5354c2
ghardard <ghardard@gmail.com>
2013-11-21 00:17:08 +0000

    Changed the test case

--- src/test/java/spacestallions/tictactoe/AssertTitleIT.java
+++ src/test/java/spacestallions/tictactoe/AssertTitleIT.java
@@ -1,22 +1,48 @@
 package spacestallions.tictactoe;
 
-import junit.framework.TestCase;
-import com.thoughtworks.selenium.DefaultSelenium;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class AssertTitleIT extends TestCase{
-	
-	protected DefaultSelenium createSeleniumClient(String url) throws Exception{
-		return new DefaultSelenium("localhost",4444,"*firefox",url);
-	}
-	
-	public void testTitle() throws Exception{
-		DefaultSelenium selenium = createSeleniumClient("http://localhost:4567");
-		selenium.start();
-		selenium.open("/");
-		selenium.waitForPageToLoad("3000");
-		assertEquals("SpaceStallions TicTacToe",selenium.getTitle());
-		selenium.stop();
-	}
+public class FirefoxTestTitleIT {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = System.getenv("STAGING_SERVER");
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
+  @Test
+  public void titleShouldBeSpaceStallionsTicTacToe() throws Exception {
+    driver.get(baseUrl);
+    assertEquals("SpaceStallions TicTacToe", driver.getTitle());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
 }
