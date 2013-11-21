81d3ad00d7b06ed0ed46f0ad9acca89fe5e4a83b
BjossiAlfreds <bjorna11@ru.is>
2013-11-21 04:18:08 +0000

    Renamed .cmd to .bat, made run.bat run the server in separate process

---
+++ bin/run.bat
@@ -0,0 +1,3 @@
+@echo off
+set PORT=4567
+start "Spark TicTacToe" java -jar target\TICTACTOE-1.0-SNAPSHOT-jar-with-dependencies.jar
--- bin/run.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set PORT=4567
-java -jar target\TICTACTOE-1.0-SNAPSHOT-jar-with-dependencies.jar
