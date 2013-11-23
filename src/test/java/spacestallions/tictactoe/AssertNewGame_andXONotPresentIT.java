a30bbb9ffab7559003f991ddc5150a43f67a1152
ghardard <ghardard@gmail.com>
2013-11-23 20:27:07 +0000

    Changed one selenium test

--- src/test/java/spacestallions/tictactoe/AssertNewGame_andXONotPresentIT.java
+++ src/test/java/spacestallions/tictactoe/AssertNewGame_andXONotPresentIT.java
@@ -12,35 +12,19 @@ import java.util.regex.Pattern;
 import static org.apache.commons.lang3.StringUtils.join;
 
 
-
-
 public class AssertNewGame_andXONotPresentIT {
-	
 	private Selenium selenium;
-	private String baseUrl;
-	private WebDriver driver;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		
-		//saucelabs
-		DesiredCapabilities caps = DesiredCapabilities.chrome();
-		caps.setCapability("platform","Linux");
-		caps.setCapability("version","30");
-		
-		//create the connection to Sauce labs to run the tests
 	
-		driver = new RemoteWebDriver(
-                new URL("http://spacestallions:8e0c8605-982f-4b17-8697-d3bec46d3599@ondemand.saucelabs.com:80/wd/hub"),
-                caps);
-
-        baseUrl = "http://spacestallions-staging.herokuapp.com/";
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://spacestallions-staging.herokuapp.com/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
 	@Test
 	public void testAssertNewGame_andXONotPresentIT() throws Exception {
-		
-		driver.get(baseUrl);
 		selenium.click("id=btnReset");
 		assertFalse(selenium.isElementPresent("X"));
 		assertFalse(selenium.isElementPresent("O"));
