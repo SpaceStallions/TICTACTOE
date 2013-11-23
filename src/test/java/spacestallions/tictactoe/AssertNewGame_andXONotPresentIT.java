38c2aa0ce9ad9b3440d3138c346e5ba64487a6a6
ghardard <ghardard@gmail.com>
2013-11-23 20:47:11 +0000

    Fixed class name in selenium test NewGame_andXONotPresent.java

---
+++ src/test/java/spacestallions/tictactoe/AssertNewGame_andXONotPresentIT.java
@@ -0,0 +1,37 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
+import static org.apache.commons.lang3.StringUtils.join;
+
+
+public class NewGame_andXONotPresentIT {
+	private Selenium selenium;
+
+	@Before
+	public void setUp() throws Exception {
+	
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://spacestallions-staging.herokuapp.com/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+	}
+
+	@Test
+	public void testAssertNewGame_andXONotPresentIT() throws Exception {
+		selenium.click("id=btnReset");
+		assertFalse(selenium.isElementPresent("X"));
+		assertFalse(selenium.isElementPresent("O"));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
